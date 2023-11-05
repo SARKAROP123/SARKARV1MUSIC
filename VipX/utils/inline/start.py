01d815fbdf83251cd5d5d8d5697c0a1a3bba6f00
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-11-05 20:08:20 +0530

    Update start.py

--- VipX/utils/inline/start.py
+++ VipX/utils/inline/start.py
@@ -13,7 +13,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="•─╼⃝𖠁𝐀𝙳𝙳 ◈ 𝐌𝙴 ◈ 𝐁𝙰𝙱𝚈𖠁⃝╾─•",
+                text="➕️•𝐀𝙳𝙳 ◈ 𝐌𝙴 ◈ 𝐁𝙰𝙱𝚈•➕️",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
@@ -38,7 +38,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="•─╼⃝𖠁𝐀𝙳𝙳 ◈ 𝐌𝙴 ◈ 𝐁𝙰𝙱𝚈𖠁⃝╾─•",
+                text="➕️•𝐀𝙳𝙳 ◈ 𝐌𝙴 ◈ 𝐁𝙰𝙱𝚈•➕️",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         
