dfcb65d82184e7b9dd47ffced985043995081812
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-11-05 19:22:57 +0530

    Update ping.py

--- VipX/plugins/ping.py
+++ VipX/plugins/ping.py
@@ -80,10 +80,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="♦️𝐆𝐑𝐎𝐔𝐏♦️", url=f"https://t.me/tg_friendss",
+                text="🍎𝐆𝐑𝐎𝐔𝐏🍎", url=f"https://t.me/TKS_CHAT_OFFICIAL",
             ),
             InlineKeyboardButton(
-                text="♦️𝐌𝐎𝐑𝐄♦️", url=f"https://t.me/vip_creators",
+                text="🍎𝐌𝐎𝐑𝐄🍎", url=f"https://t.me/TKS_JOIN",
             )
         ],
         [
