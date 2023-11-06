0ccd03cbcafcce878c61a62f0ecae204e99b0091
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-11-06 09:06:22 +0530

    Update start.py

--- VipX/utils/inline/start.py
+++ VipX/utils/inline/start.py
@@ -1,52 +1,60 @@
 from typing import Union
-
+import re
+import os
+from os import getenv
+from dotenv import load_dotenv
+from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 import config
+from config import GROUP_USERNAME, CHANNEL_USERNAME
 
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="➕𝗔∂∂ 𝗠є 𝗬συя 𝗚яσυρ➕",
+                text="•𝐀𝙳𝙳 ◈ 𝐌𝙴 ◈ 𝐁𝙰𝙱𝚈•",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="✰𝗛𝗘𝗟𝗣✰",
+                text="🦋𝐅𝐄𝐀𝐓𝐔𝐑𝐄🦋",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="★𝗦𝗘𝗧𝗧𝗜𝗡𝗚★", callback_data="settings_helper"
+                text="⚙️𝐒𝐄𝐓𝐓𝐈𝐍𝐆⚙️", callback_data="settings_helper"
             ),
         ],
      ]
     return buttons
 
+#extra shit
+BOT_USERNAME = ("{BOT_USERNAME}")
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    global GROUP_USERNAME
+    global CHANNEL_USERNAME
     buttons = [
         [
             InlineKeyboardButton(
-                text="➕𝗔∂∂ 𝗠є 𝗬συя 𝗚яσυρ➕",
+                text="•𝐀𝙳𝙳 ◈ 𝐌𝙴 ◈ 𝐁𝙰𝙱𝚈•",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
+        
         ],
         [
             InlineKeyboardButton(
-                text="★𝗛𝗘𝗟𝗣★", callback_data="settings_back_helper"
-            )
+                text="𝐌ᴏʀᴇ🥀", url=f"https://t.me/{CHANNEL_USERNAME}",
+            ),
+        
+            InlineKeyboardButton(
+                text="𝐆𝚁𝙾𝚄𝙿✨", url=f"https://t.me/{GROUP_USERNAME}",
+            ),
         ],
         [
             InlineKeyboardButton(
-                text="★𝗦𝗨𝗣𝗣𝗢𝗥𝗧★", url=config.SUPPORT_GROUP
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="★𝗢𝗪𝗡𝗘𝗥★", user_id=OWNER 
+                text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper"
             )
         ],
      ]
